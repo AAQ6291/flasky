54e01561105f2e7fa1eef4ff693d736a584d683e
angela_kuo <angelak.tw@gmail.com>
2018-11-20 16:53:25 +0800

    20181120

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from flask import make_response  # 回傳一個回應物件
+from flask import abort          # 處理錯誤狀態
+
 app = Flask(__name__)
 
 
@@ -6,10 +9,22 @@ app = Flask(__name__)
 def index():
     # 瀏覽器資訊
     user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser id {}</p>'.format(user_agent)
+    # return '<p>Your browser id {}</p>'.format(user_agent)
     # return '<h1>Hello World!</h1>'
+
+    # 使用轉譯模板, 附帶參數傳送
+    return render_template('index.html', user_agent=user_agent)
 
 
 @app.route('/user/<name>')     # 動態路由
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+def get_user(name):
+    user = load_user(name)
+    if not user:
+        abort(404)
+    return render_template('user.html', name=name)
+
+
+def load_user(name):
+    # return '<h1>Hello, {}!</h1>'.format(name)
+    # return render_template('user.html', name=name)
+    return name
