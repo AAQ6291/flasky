87f07db8f9c17982d826e77d62bb5dc91c532a44
angela_kuo <angelak.tw@gmail.com>
2018-11-27 17:12:46 +0800

    20181127

--- hello.py
+++ hello.py
@@ -1,17 +1,19 @@
 from flask import Flask, request, render_template
 from flask import make_response  # 回傳一個回應物件
-from flask import abort          # 處理錯誤狀態
-from flask_script import Manager  # 提供外不指令
+
 
 # Flash_boostrap 包含所有Bootstrap檔案 & 基礎結構模板
 from flask_bootstrap import Bootstrap
+# Flask_moment 解析、驗證、操作、格式化日期
 from flask_moment import Moment
+# 時間
+from datetime import datetime
 
 app = Flask(__name__)
-manager = Manager(app)
-moment = Moment(app)
+
 # app 利用 Jinja2 的模板繼承並擴充此套件
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 
 @app.errorhandler(404)
@@ -29,14 +31,13 @@ def index():
     # 瀏覽器資訊
     user_agent = request.headers.get('User-Agent')
 
+    # 傳送時間 current_time
+    current_time = datetime.utcnow()
+
     # 使用轉譯模板, 附帶參數傳送
-    return render_template('index.html', user_agent=user_agent)
+    return render_template('index.html', user_agent=user_agent, current_time=current_time)
 
 
 @app.route('/user/<name>')     # 動態路由
 def user(name):
     return render_template('user.html', name=name)
-
-
-if __name__ == '__main__':
-    manager.run()
