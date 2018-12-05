7fc31209fcc536d40e7fbd39e8f2573a81c861b3
angela_kuo <angelak.tw@gmail.com>
2018-12-05 17:51:45 +0800

    20181205-sqllachemy2

--- hello.py
+++ hello.py
@@ -28,11 +28,12 @@ db = SQLAlchemy(app)
 
 
 class Role(db.Model):
+    # 模型定義
     # Define Role Modle's (角色) Table
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)  # set pkey=id
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role')  # 關係連結
 
     def __repr__(self):
         return '<Role %r>' % self.name
@@ -43,6 +44,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    # 連結到<roles>.<id>
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
     def __repr__(self):
         return '<User %r>' % self.username
